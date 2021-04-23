bfa6a90ef186415e91fe4fc08a19847f6af9f318
herick lima <celebrogamer@gmail.com>
2021-04-23 19:07:01 -0300

    finished the factorial code

--- factorial.py
+++ factorial.py
@@ -1,7 +1,31 @@
-def fatorial(num):
-    ante = num
-    prox = a
+def fatorial(num, escolha):
+    prox = 1
 
-    for i in range(num, -1, -1):
+    if escolha == '2':
+        if num == 0:
+            return 1
+        else:
+            for i in range(num):
+                prox = prox * (i + 1)
         
+        return prox
+    else:
+        if num == 0:
+            return 1
+        else:
+            for i in range(num):
+                prox = prox * (i + 1)
+                print("{}! = {}".format(i + 1, prox))
 
+    
+def main():
+    print("Fatorial!")
+    print("Ver passo a passo?")
+    escolha = input("1 = S\n2 = N\n")
+    num = int(input("Digite o numero: "))
+    if escolha == '2':
+        print("{}! = {}".format(num, fatorial(num, escolha)))
+    else:
+        fatorial(num, escolha)
+
+main()
